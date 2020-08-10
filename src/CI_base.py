6f10e540f4dbd378a0fe06db01e3d779dfb79b5f
Lin <13871876+tianlinxu312@users.noreply.github.com>
2020-08-10 10:45:56 +0100

    Update CI_base.py
    fix error:  NameError: global name 'accuracy_score' is not defined

--- src/CI_base.py
+++ src/CI_base.py
@@ -103,16 +103,16 @@ class CI_base(object):
             gbm = model
             pred = gbm.predict_proba(torch.from_numpy(Xtest).float())
             pred_exact = np.argmax(pred,axis =1)
-            acc1 = accuracy_score(Ytest, pred_exact)
-            AUC1 = roc_auc_score(Ytest,pred[:,1])
+            acc1 = sklearn.metrics.accuracy_score(Ytest, pred_exact)
+            AUC1 = sklearn.metrics.roc_auc_score(Ytest,pred[:,1])
             del gbm
             model = MLP(params,Xtrain[:,self.dx::].shape[1],2,cudaEfficient=True)
             model.fit(Xtrain[:,self.dx::],Ytrain.astype(int),validation_split=0.2)
             gbm = model
             pred = gbm.predict_proba(torch.from_numpy(Xtest[:,self.dx::]).float())
             pred_exact = np.argmax(pred,axis =1)
-            acc2 = accuracy_score(Ytest, pred_exact)
-            AUC2 = roc_auc_score(Ytest,pred[:,1])
+            acc2 = sklearn.metrics.accuracy_score(Ytest, pred_exact)
+            AUC2 = sklearn.metrics.roc_auc_score(Ytest,pred[:,1])
             print 'Using Deep Model: ',
             print gbm
             del gbm
@@ -129,8 +129,8 @@ class CI_base(object):
             gbm = model.fit(Xtrain[:,self.dx::],Ytrain)
             pred = gbm.predict_proba(Xtest[:,self.dx::])
             pred_exact = np.argmax(pred,axis =1)
-            acc2 = accuracy_score(Ytest, pred_exact)
-            AUC2 = roc_auc_score(Ytest,pred[:,1])
+            acc2 = sklearn.metrics.accuracy_score(Ytest, pred_exact)
+            AUC2 = sklearn.metrics.roc_auc_score(Ytest,pred[:,1])
             print 'Using XGB model: '
             print gbm
             del gbm
